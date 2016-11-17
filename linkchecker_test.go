bec947b0d241abbe2079a06e601f4d46316b0926
Gregorio Martinez <greg@gmartinez.com>
2016-11-17 11:51:19 -0500

    Resync :|

---
+++ linkchecker_test.go
@@ -0,0 +1,11 @@
+package main
+
+import (
+	"testing"
+)
+
+func TestReadFile(t *testing.T) {
+	t.Error("Testing")
+}
+
+//@TODO add test for x-robots-tag
--- main.go
+++ main.go
@@ -8,15 +8,13 @@ import (
 	"log"
 	"net/http"
 	"os"
+	"path"
 	"strings"
-	"sync"
 )
-
-var wg sync.WaitGroup
 
 func main() {
 	// Open file
-	var filename = flag.String("file", "urls.txt", "File to check")
+	var filename = flag.String("file", "", "File to check")
 	flag.Parse()
 
 	file, err := os.Open(*filename)
@@ -26,28 +24,54 @@ func main() {
 	defer file.Close()
 
 	// Read File
-
 	lines, err := readFile(file)
 	if err != nil {
 		log.Fatalf(err.Error())
 	}
 
-	fmt.Println(lines)
+	for _, link := range lines {
+		resp, err := fetch(link)
+		if err != nil {
+			fmt.Println(err)
+			continue
+		}
 
+		// @TODO test this
+		robots := resp.Header.Get("X-Robots-Tag")
+
+		fmt.Println(resp.Status, link, robots)
+
+	}
 }
 
-func fetch(link string, ch chan<- string) (*http.Response, error) {
+func fetch(link string) (*http.Response, error) {
 	resp, err := http.Head(link)
 	if err != nil {
-		log.Fatal(err)
+		// log.Fatal(err)
 	}
 	return resp, err
 }
 
 // readFile reads in a file and returns a slice of strings with the spaces removed
+// @TODO make sure valid url
+// @TODO make relative to absolute
+// Reads in txt files right now. Expand to txt, csv, xml
+// @TODO don't just trust file extension
 func readFile(file *os.File) ([]string, error) {
 	name := file.Name()
+
 	log.Printf("Reading file: %s \n", name)
+
+	extension := path.Ext(name)
+
+	switch extension {
+	case ".txt":
+		fmt.Println("txt file loaded")
+	case ".xml":
+		fmt.Println("XML document loaded")
+	default:
+		fmt.Println("unsupported file extension")
+	}
 
 	scanner := bufio.NewScanner(file)
 	var lines []string
